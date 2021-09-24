9a3d8edb0f37a9c056a4130a25ac3373499429de
masterglob <masterglob@gmail.com>
2021-09-24 17:56:51 +0200

    Add todo list

--- _dev/src/pbkr_version.h
+++ _dev/src/pbkr_version.h
@@ -1,11 +1,19 @@
 #pragma once
 
-#define PBKR_VERSION "1.0.2"
+#define PBKR_VERSION "1.0.3"
 extern const int PBKR_BUILD_ID;
 
 /*
  New in V1.0.2:
    - Add "negative" latency (applied to MIDI instead of samples)
    - Add version & build on "config WEB page" + refactor tabs
+ New in V1.0.3:
+   - TODO
+
+ Known bugs:
+   - Latency & ESP self-test do not takie into account actual
+       sample rate (44100/48000)
+   - Default MIDI volume is not applied until it is manually changed
+
  */
 
