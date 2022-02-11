5c41d37dcb4a71a099488e328d0dd53b96df58b6
masterglob <masterglob@gmail.com>
2022-02-11 17:13:54 +0100

    Update for RPI3B+ and Tinycore V13.0

--- _target/opt/bootlocal.sh
+++ _target/opt/bootlocal.sh
@@ -1,29 +1,38 @@
 #!/bin/sh
 
-# Start serial terminal
+# To start serial terminal /w console
+# Add the following to the cmdline.txt console=serial0,115200
+# Reference https://www.raspberrypi.org/documentation/configuration/uart.md for UART configuration
+# Uncomment the next line
 # /usr/sbin/startserialtty &
 
 # Set CPU frequency governor to ondemand (default is performance)
 echo ondemand > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 
-# Load modules
+# Load modules - i2c-dev needs manually loaded even if enabled in config.txt
 /sbin/modprobe i2c-dev
 /sbin/modprobe snd_soc_pcm5102a
 
-# ------ Put other system startup commands below this line
 loadkmap < /usr/share/kmap/azerty/fr-latin1.kmap
+
+if [ -f /var/run/udhcpc.eth0.pid ]; then
+kill `cat /var/run/udhcpc.eth0.pid`
+sleep 0.1
+fi
+
+# Start openssh daemon
 /usr/local/etc/init.d/openssh start &
 
 route add default gw 192.168.1.1 eth0
-ifconfig eth0 192.168.7.80 netmask 255.255.255.0
 ifconfig wlan0 up
 ifconfig wlan0 192.168.22.1 netmask 255.255.255.0
-/usr/local/bin/hostapd -B /usr/local/etc/hostapd.conf
-udhcpd /etc/udhcpd.conf
+#/usr/local/bin/hostapd -B /usr/local/etc/hostapd.conf
+#udhcpd /etc/udhcpd.conf
 
-sudo date -s 2020.05.20-12:00
+ifconfig eth0 192.168.7.80 netmask 255.255.255.0
 
-ld environment
+# ------ Put other system startup commands below this line
+ls environment
 mkdir -p /tmp/_dev
 chmod 777 /tmp/_dev
 
@@ -37,10 +46,11 @@ chmod -R 777 /home/www
 # configure serial link
 stty -F /dev/ttyAMA0  115200 -evenp
 
-# start application
-(sleep 2 && cd /root/pbkr && ./pbkr.sh)
-echo "" > /usr/local/apache2/htdocs/index.html
+#httpd
+#echo "" > /usr/local/apache2/htdocs/index.html
 
-# To start samba when loaded : smbd
-httpd
+ifconfig
+
+# start application
+cd /root/pbkr && ./pbkr.sh
 
--- _target/opt/bootsync.sh
+++ _target/opt/bootsync.sh
@@ -3,4 +3,5 @@
 # Use bootlocal.sh for system startup commands that can run in the background 
 # and therefore not slow down the boot process.
 /usr/bin/sethostname wtplayer
+loadkmap < /usr/share/kmap/azerty/fr.kmap
 /opt/bootlocal.sh &
